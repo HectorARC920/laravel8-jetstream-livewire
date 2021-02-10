7dfd6b685b0c6fa6d59931c09bfd5410385dc490
Hector Rubio <hector.rubio.920@hotmail.com>
2021-02-10 00:22:22 -0700

    detalle de curso

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Course;
 class PageController extends Controller
 {
     public function home()
     {
          return view('index');
     }
-    public function course()
+    public function course(Course $course)
     {
-        return view('course');
+        return view('course',compact('course'));
     }
 }
