8330c8735ad408e98d1e25f3eb3da4e0673ee90d
Hector Rubio <hector.rubio.920@hotmail.com>
2021-02-09 18:32:30 -0700

    Se diseño un componente que agrupa logica y diseño course list

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -13,4 +13,8 @@ class Course extends Model
     {
         return $this->BelongsTo(User::class); 
     }
+    public function getExcerptAttribute()
+    {
+        return substr($this->description,0,80) . "...";
+    }
 }
